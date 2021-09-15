bb843918929b8491532d8b93e16601f09050ad1c
Fahed Bakar <fahedbakar@hotmail.de>
2021-09-15 22:38:55 +0200

    Generate ID

--- src/main/java/com/fahed/forms/entities/Form.java
+++ src/main/java/com/fahed/forms/entities/Form.java
@@ -6,6 +6,8 @@ import lombok.NoArgsConstructor;
 import org.springframework.lang.NonNull;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Data
@@ -14,6 +16,7 @@ import javax.persistence.Id;
 public class Form {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String id;
 
     @NonNull
