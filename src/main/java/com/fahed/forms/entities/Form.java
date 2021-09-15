1b095333b33b802111f351fd232547e4abf07234
Fahed Bakar <fahedbakar@hotmail.de>
2021-09-15 22:32:45 +0200

    Add 10 questions

--- src/main/java/com/fahed/forms/entities/Form.java
+++ src/main/java/com/fahed/forms/entities/Form.java
@@ -22,5 +22,15 @@ public class Form {
     @NotNull
     private String lastName;
 
+    private String question1;
+    private String question2;
+    private String question3;
+    private String question4;
+    private String question5;
+    private String question6;
+    private String question7;
+    private String question8;
+    private String question9;
+    private String question10;
 
 }
